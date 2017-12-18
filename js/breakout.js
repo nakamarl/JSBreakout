c7422346ab315bde407352b9c68f6c48d8405660
s17007 <s17007@std.it-college.ac.jp>
2017-12-18 11:19:12 +0900

    一応動く(バグあり)

--- js/breakout.js
+++ js/breakout.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
-    console.log("breakout initializing...");
-
+    // 初期化
     const canvas = document.getElementById('board');
     new Breakout({
         canvas: canvas,
-        interval: 1000 / 60,
+        interval: 1000 / 60,    // 60 FPS
         paddle: {width: 100,height: 10,color: '#FFFFFF'},
-        ball: {radius: 5, color: '#FFFFFF'}
+        ball: {radius: 5, color: '#FFFFFF'},
+        block: {width: 80,height: 20}
     });
 });
 
@@ -32,7 +32,7 @@ class Breakout {
     }
 
     static setGameOver(f) {
-        if(f instanceof Boolean) {
+        if (f instanceof Boolean) {
             Breakout._game_over = f;
             return;
         }
@@ -53,10 +53,14 @@ class Breakout {
         this.paddle.setPosition(Breakout.width / 2, Breakout.height * 8 / 9);
         this.paddle.setSpeed(Breakout.width / 100);
 
+        this.blockManager = new BlockManager(options.block.width, options.block.height);
+        this.blockManager.stage1();
+
         this.ball = new Ball(options.ball.radius, options.ball.color);
         this.ball.setPosition(Breakout.width / 2, Breakout.height / 2);
 
         this.ball.addTarget(this.paddle);
+        this.ball.addTarget(this.blockManager.blockList);
 
         setInterval(this.draw.bind(this), options.interval);
 
@@ -67,11 +71,9 @@ class Breakout {
     keydown(evt) {
         if (evt.code === 'ArrowLeft') {
             this.leftKey = true;
-        }
-        else if (evt.code === 'ArrowRight') {
+        } else if (evt.code === 'ArrowRight') {
             this.rightKey = true;
-        }
-        else if (evt.code === 'Space') {
+        } else if (evt.code === 'Space') {
             this.ball.setSpeed(5, 135);
         }
     }
@@ -79,8 +81,7 @@ class Breakout {
     keyup(evt) {
         if (evt.code === 'ArrowLeft') {
             this.leftKey = false;
-        }
-        else if (evt.code === 'ArrowRight') {
+        } else if (evt.code === 'ArrowRight') {
             this.rightKey = false;
         }
     }
@@ -93,21 +94,20 @@ class Breakout {
         if (this.rightKey) {
             this.paddle.moveRight();
         }
-        if(Breakout.isGameOver) {
+        if (Breakout.isGameOver) {
             this.context.save();
 
-            this.context.strokeStyle = "red";
-            this.context.fillStyle = "#FFFFFF";
+            this.context.fillStyle = "red";
             this.context.font = "48pt Arial";
             this.context.textAlign = "center";
-            this.context.fillText("GameOver",Breakout.width / 2,Breakout.height / 2);
-            this.context.strokeText("GameOver",Breakout.width / 2,Breakout.height / 2);
+            this.context.fillText("GameOver", Breakout.width / 2, Breakout.height / 2);
+
             this.context.restore();
-        }
-        else {
+        } else {
             this.ball.draw(this.context);
         }
         this.paddle.draw(this.context);
+        this.blockManager.draw(this.context);
     }
 }
 
@@ -128,7 +128,7 @@ class Entity {
         ]
     }
 
-    hit() {
+    hit(ball) {
     }
 }
 
@@ -154,16 +154,17 @@ class Paddle extends Entity {
         context.restore();
     }
 
-
     setPosition(x, y) {
         this.x = x;
         this.y = y;
         this.fixPosition();
     }
 
+
     setSpeed(speed) {
         this.speed = speed;
     }
+
 
     moveLeft() {
         this.x -= this.speed;
@@ -187,14 +188,102 @@ class Paddle extends Entity {
         }
     }
 
+
     hit(ball) {
-        if(this.x + this.width / 4 < ball.x){
+        if (this.x + this.width / 4 < ball.x) {
             ball.changeAngle();
             return;
         }
-        if(this.x - this.width / 4 > ball.x){
+        if (this.x - this.width / 4 > ball.x) {
             ball.changeAngle(true);
         }
+    }
+}
+
+class Block extends Entity {
+    static get colorSet() {
+        return [
+            ['Pink', 'Crimson'],
+            ['HotPink', 'DeepPink'],
+            ['Violet', 'Magenta'],
+            ['MediumOrchid', 'DarkOrchid'],
+            ['MediumSlateBlue', 'DarkSlateBlue'],
+            ['Blue', 'MidnightBlue'],
+            ['LightSkyBlue', 'DeepSkyBlue'],
+            ['Cyan', 'DarkCyan'],
+            ['MediumAquamarine', 'MediumSpringGreen'],
+            ['SpringGreen', 'SeaGreen'],
+            ['DarkGreen', 'LawnGreen'],
+            ['Yellow', 'Olive'],
+            ['Gold', 'DarkGoldenrod'],
+            ['Orange', 'DarkOrange'],
+            ['Coral', 'OrangeRed'],
+            ['Red', 'DarkRed'],
+        ];
+    }
+
+    constructor(manager, x, y, width, height, color) {
+        super();
+        this.manager = manager;
+        this.width = width;
+        this.height = height;
+        if (color >= Block.colorSet.length) {
+            color = Block.colorSet.length - 1;
+        }
+        this.color = Block.colorSet[color];
+        this.x = x;
+        this.y = y;
+    }
+
+    draw(context) {
+        context.save();
+
+        context.translate(this.x, this.y);
+        context.fillStyle = this.color[0];
+        context.fillRect(-(this.width / 2), -(this.height / 2),
+            this.width, this.height);
+        context.lineWidth = 4;
+        context.strokeStyle = this.color[1];
+        context.strokeRect(-(this.width / 2) + 2, -(this.height / 2) + 2,
+            this.width - 4, this.height - 4);
+
+        context.restore();
+    }
+
+    hit(ball) {
+        ball.removeTarget(this);
+        this.manager.removeTarget(this);
+    }
+}
+
+class BlockManager {
+    constructor(baseWidth, baseHeight) {
+        this.baseWidth = baseWidth;
+        this.baseHeight = baseHeight;
+        this.blockList = [];
+    }
+
+    stage1() {
+        for (let x = 0; x < 7; x++) {
+            for (let y = 0; y < 6; y++) {
+                const color = parseInt(Math.random() * Block.colorSet.length);
+                this.blockList.push(
+                    new Block(this, this.baseWidth * (x + 1)
+                        , this.baseHeight * (y + 1),
+                        this.baseWidth, this.baseHeight, color));
+            }
+        }
+    }
+
+    removeTarget(object) {
+        this.blockList.splice(this.blockList.indexOf(object), 1);
+    }
+
+
+    draw(context) {
+        this.blockList.forEach((block) => {
+            block.draw(context);
+        }, this);
     }
 }
 
@@ -211,17 +300,23 @@ class Ball {
 
     addTarget(object) {
         if (Array.isArray(object)) {
-            this.targetList.concat(object);
-        }
-        else {
+            for (let i in object) {
+                this.targetList.push(object[i]);
+            }
+        } else {
             this.targetList.push(object);
         }
+    }
+
+    removeTarget(object) {
+        this.targetList.splice(this.targetList.indexOf(object), 1);
     }
 
     setPosition(x, y) {
         this.x = x;
         this.y = y;
     }
+
 
     setSpeed(speed, direction) {
         const rad = direction * Math.PI / 180;
@@ -241,6 +336,9 @@ class Ball {
     collision() {
         let isCollision = false;
         this.targetList.forEach((target) => {
+            if (isCollision) {
+                return false;
+            }
             const points = target.getCornerPoints();
             points.forEach((point) => {
                 const a = Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
@@ -250,6 +348,9 @@ class Ball {
                 }
             }, this);
 
+            if (isCollision) {
+                return false;
+            }
             const bl = this.x - this.radius;
             const br = this.x + this.radius;
             const bt = this.y - this.radius;
@@ -257,7 +358,6 @@ class Ball {
             if (points[0].x < br && bl < points[1].x) {
                 if (points[0].y < bb && bt < points[2].y) {
                     isCollision = true;
-                    this.y -= bb - points[0].y;
                     target.hit(this);
                 }
             }
@@ -265,17 +365,17 @@ class Ball {
         return isCollision;
     }
 
-    changeAngle(ccw = false){
+    changeAngle(ccw = false) {
         let theta = Math.atan(this.dy / this.dx);
         const speed = this.dx / Math.cos(theta);
-        if(ccw){
+        if (ccw) {
             theta -= Math.PI * 5 / 180;
-        }
-        else{
+        } else {
             theta += Math.PI * 5 / 180;
         }
 
-        if(theta <= -0.7853981634 || theta >= 0.5235987756){
+        if (theta <= -0.7853981634 || theta >= 0.5235987756) {
+            // 変更なしにする
             return;
         }
         this.dx = Math.cos(theta) * speed;
@@ -313,6 +413,7 @@ class Ball {
     reflectionY() {
         this.dy *= -1;
     }
+
 
     draw(context) {
         this.move();
